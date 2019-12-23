b4936f9faf8ec223e60aa94c8160dd9df8258ca2
Ethan Fritz <ethanfritz.t@gmail.com>
2019-12-22 20:38:02 -0500

    optimized

--- index.php
+++ index.php
@@ -1,21 +1,14 @@
 <!DOCTYPE html>
+<?php //header('Location: task.php'); ?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link type="text/css" rel="stylesheet" href="./css/style.css">
-    <link rel="icon" href="./img/taskrIcon.ico">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    
+  </head>
     <title>taskr</title>
   </head>
+    <?php require_once 'task.php'; ?>
   <body>
-    <form class="forminput" action="index.php" method="POST">
-      <input class="inputbox" type="text" placeholder="What's Your Main Focus for Today" name="input-box" autocomplete="off" required>
-    </form>
-    <!-- seperate form so the enter key doesn't delete tasks -->
-    <form class= "tasklist-form" action="index.php" method="POST">
-      <ul class='list-container' name='field'>
-        <?php require 'main.php'; ?>
-      </ul>
-  </form>
+    <img class="index-logo" src="./img/taskrLogo.png">
   </body>
 </html>
--- task.php
+++ task.php
@@ -1,14 +1,38 @@
 <!DOCTYPE html>
-<!-- this page is simply so the previous post request doesn't submit twice -->
-<!-- will be used for submiting and deleting to and from the db -->
+<!-- this page will hold the navbar and the linking sheets so i can customise index.php -->
+<?php //require 'backendDay.php'; ?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link type="text/css" rel="stylesheet" href="./css/style.css">
     <link rel="icon" href="./img/taskrIcon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
     <title>taskr</title>
   </head>
+  <header>
+    <nav class="navbar">
+      <ul class="navbar-container">
+        <li class="navbar-list"><a class="navbar-link" href="index.php">Home</a></li>
+        <li class="navbar-list"><a class="navbar-link" href="daily.php">Daily</a></li>
+        <li class="navbar-list"><a class="navbar-link" href="monthly.php">Monthly</a></li>
+        <li class="navbar-list"><a class="navbar-link" href="yearly.php">Yearly</a></li>
+      </ul>
+    </nav>
+    <div class="title-page">
+      <?php
+        if(basename($_SERVER['PHP_SELF']) == 'daily.php'){
+          echo '<p class="p-title">Daily Tasks!</p>';
+        }elseif(basename($_SERVER['PHP_SELF']) == 'monthly.php'){
+          echo '<p class="p-title">Monthly Tasks!</p>';
+        }elseif(basename($_SERVER['PHP_SELF']) == 'yearly.php'){
+          echo '<p class="p-title">Yearly Tasks!</p>';
+        }else{
+          echo '<p class="p-title">Home</p>';
+        }
+      ?>
+    </div>
+  </header>
   <body>
-    <p class="text">Submitted</p>
-    <?php  header('Location: index.php');?>
+    <?php //header('Location: index.php'); ?>
   </body>
 </html>
